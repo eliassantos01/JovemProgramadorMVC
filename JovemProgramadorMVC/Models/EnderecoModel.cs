0f45b5620a120169ff8f157b5f586c01814ebf8d
eliassantos01 <eliasdossantos2010@gmail.com>
2022-11-23 20:50:43 -0300

    Implementando API

---
+++ JovemProgramadorMVC/Models/EnderecoModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JovemProgramadorMVC.Models
+{
+    public class EnderecoModel
+    {
+        public string Logradouro { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Uf { get; set; }
+
+        public string Ddd { get; set; }
+
+    }
+}
